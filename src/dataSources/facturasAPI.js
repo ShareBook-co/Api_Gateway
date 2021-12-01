5236ac5efa9d7debc4e739e2e5b093735cf23d94
jgabriel2g <jgabriel2g@hotmail.com>
2021-12-01 11:42:06 -0500

    P17G3-30: Create Api Gateway's Data Sources

---
+++ src/dataSources/facturasAPI.js
@@ -0,0 +1,56 @@
+const { RESTDataSource } = require('apollo-datasource-rest');
+const serverConfig = require('../server')
+
+class facturasAPI extends RESTDataSource {
+    constructor(){
+        super();
+        this.baseUrl = serverConfig.facturasAPIurl;
+    }
+
+    async createCliente(cliente){
+        cliente = new Object( JSON.parse ( JSON.stringify(cliente) ));
+        return await this.post('/clientes', cliente);
+    }
+    async updateCliente(cliente){
+        cliente = new Object( JSON.parse ( JSON.stringify(cliente) ));
+        return await this.put('/clientes/update', cliente);
+    }
+    async deleteCliente(username){ 
+        return await this.delete(`/clientes/delete/${username}`);
+    }
+    async clienteByUsername(username) {
+        return await this.get(`/clientes/${username}`)
+    }
+    async createVendedor(vendedor) {
+        vendedor = new Object( JSON.parse ( JSON.stringify(vendedor) ));
+        return await this.post('/vendedores', vendedor);
+    }
+    async updateVendedor(vendedor) {
+        vendedor = new Object( JSON.parse ( JSON.stringify(vendedor) ));
+        return await this.put('/vendedores/update', vendedor);
+    }
+    async deleteVendedor(username) { 
+        return await this.delete(`/vendedores/delete/${username}`)
+    }
+    async vendedorByUsername(username) {
+        return await this.get(`/vendedores/${username}`);
+    }
+    async createFactura(factura){
+        factura = new Object( JSON.parse ( JSON.stringify(factura) ));
+        return await this.post('/facturacion',);
+    }
+    async updateFactura(factura){
+        factura = new Object( JSON.parse ( JSON.stringify(factura) ));
+        return await this.put('/facturacion/update',factura);
+    }
+    async deleteFactura(facturaId){
+        return await this.delete(`/facturacion/delete/${facturaId}`);
+    }
+    async FacturasByCliente(username) {
+        return await this.get(`/facturacion/clientes/${username}`);
+    }
+    async FacturasByVendedor(username){
+        return await this.get(`/facturacion/vendedor/${username}`);
+    }
+
+}
